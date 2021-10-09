d73824cfbe8a0b9be65b5b5da6751968bd1e21a3
Felipe Rogério <feliperogerio.ti@gmail.com>
2021-10-09 12:10:41 -0300

    add-navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,47 @@ function typeWrite(elemento){
 }
 const titulo = document.querySelector('.nome');
 typeWrite(titulo);
+
+class NavBar {
+  constructor(menu, navList, navLinks) {
+    this.menu = document.querySelector(menu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+      });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.menu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.menu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.menu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const navbar = new NavBar(
+  ".menu",
+  ".nav-list",
+  ".nav-list li",
+);
+
+navbar.init();
